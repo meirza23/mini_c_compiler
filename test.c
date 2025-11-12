29ad7f436e6b0890e8a76df16530814bad9a725b
meirza23 <mirzaetka23@gmail.com>
2025-11-12 14:09:19 +0000

    .

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    float y = 2.5;
+
+    if (x > y) {
+        printf("X daha büyük\n");
+    } else {
+        printf("Y daha büyük\n");
+    }
+
+    return 0;
+}
